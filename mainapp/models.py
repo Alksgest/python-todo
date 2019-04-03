5c571c41a5f90bbc64b267eaa50e7773b449d03b
Владислав Кириченко <vladislav.kirichenko@syncopate.local>
2019-04-03 16:43:36 +0300

    added possibility to create todos

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,5 +3,4 @@ from django.db import models
 class TodoModel(models.Model):
     date = models.DateField(verbose_name="Date")
     content = models.TextField(verbose_name='Content')
-    
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,29 +1,45 @@
+from django.db import models
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render
+from django.views.generic import FormView, TemplateView
 
-from django.views.generic import TemplateView, FormView
-from django.http import HttpResponse
 from .forms import TodoInputForm
 from .models import TodoModel
-from django.db import models
+
 
 class AddTodoView(FormView):
     template_name = "addTodo.html"
     form_class = TodoInputForm
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         form = self.form_class(initial = self.initial)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            return HttpResponse("succses")
+            item = TodoModel()
+            item.date = request.POST.get("date")
+            item.content = request.POST.get("content")
+            item.save()
+            # d = request.POST.get("date")
+            # c = request.POST.get("content")
+            # TodoModel.objects.create(date=d, content=c)
+            return HttpResponsePermanentRedirect("/")
         
         return HttpResponse("not success.(")
+
 
 class HomeView(FormView):
     template_name = "index.html"
 
-    def get(self, request):
-        data = TodoModel.fiel.all()
-        return render(request, self.template_name, context=data)
+    def get(self, request, *args, **kwargs):
+        #data = TodoModel.objeсts.all()
+        # data = {
+        #     "data" : [TodoModel(), TodoModel()]
+        # }
+        # data = { 
+        #     "data" : TodoModel.objects.all()
+        # }
+        data = TodoModel.objects.all()
+        return render(request, self.template_name, {"data" : data})
